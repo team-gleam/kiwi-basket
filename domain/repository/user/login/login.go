b6981fc75e1a67b79cb815d19564bc9952938023
4afS <4afS.h1@gmail.com>
2020-03-16 11:44:26 +0900

    find by username

--- domain/repository/user/login/login.go
+++ domain/repository/user/login/login.go
@@ -1,9 +1,13 @@
 package login
 
-import "github.com/the-gleam/kiwi-basket/domain/model/user/login"
+import (
+	"github.com/the-gleam/kiwi-basket/domain/model/user/login"
+	"github.com/the-gleam/kiwi-basket/domain/model/user/username"
+)
 
 type ILoginRepository interface {
 	Create(login.Login) error
 	Delete(login.Login) error
-	Exists(login.Login) (bool, error)
+	Exists(username.Username) (bool, error)
+	Get(username.Username) (login.Login, error)
 }

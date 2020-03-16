20ad31bb0ebf5af1f2364837b083b16ff537eda3
4afS <4afS.h1@gmail.com>
2020-03-16 10:34:19 +0900

    add a login to the repository

---
+++ usecase/user/login/login.go
@@ -0,0 +1,30 @@
+package login
+
+import (
+	"fmt"
+
+	loginModel "github.com/the-gleam/kiwi-basket/domain/model/user/login"
+	loginRepository "github.com/the-gleam/kiwi-basket/domain/repository/user/login"
+)
+
+type LoginUsecase struct {
+	loginRepository loginRepository.ILoginRepository
+}
+
+func NewLoginUsecase(r loginRepository.ILoginRepository) LoginUsecase {
+	return LoginUsecase{r}
+}
+
+func (u LoginUsecase) Add(l loginModel.Login) error {
+	exist := u.loginRepository.Exists(l)
+	if exist {
+		return fmt.Errorf("this username already exists")
+	}
+
+	err := u.loginRepository.Create(l)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

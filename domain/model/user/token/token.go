47b1bd3004d7000363d4a4dff2651a508109e20c
4afS <4afS.h1@gmail.com>
2020-03-16 09:07:42 +0900

    generate a token which's a random hex number

--- domain/model/user/token/token.go
+++ domain/model/user/token/token.go
@@ -1,11 +1,32 @@
 package token
 
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
 type Token struct {
 	token string
 }
 
 func NewToken(t string) Token {
 	return Token{t}
+}
+
+func GenToken() (Token, error) {
+	n, err := rand.Int(rand.Reader, max())
+	if err != nil {
+		return NewToken(""), err
+	}
+	return NewToken(fmt.Sprintf("%x", n)), nil
+}
+
+func max() *big.Int {
+	x := big.NewInt(2)
+	y := big.NewInt(80)
+	n := new(big.Int).Exp(x, y, nil)
+	return n // 0x100000000000000000000
 }
 
 func (t Token) Token() string {

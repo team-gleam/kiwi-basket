b4c55109343d33309df617a849e64e621a6a594d
4afS <4afS.h1@gmail.com>
2020-04-24 13:40:09 +0900

    separate a error

--- domain/model/user/username/username.go
+++ domain/model/user/username/username.go
@@ -2,13 +2,17 @@ package username
 
 import "fmt"
 
+const (
+	InvalidUsername = "invalid empty string"
+)
+
 type Username struct {
 	name string
 }
 
 func NewUsername(u string) (Username, error) {
 	if u == "" {
-		return Username{}, fmt.Errorf("invalid empty string")
+		return Username{}, fmt.Errorf(InvalidUsername)
 	}
 	return Username{u}, nil
 }

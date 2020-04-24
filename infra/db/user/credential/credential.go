c2e51b25f9a4adbff949ba082c6156a931c7f3ff
4afS <4afS.h1@gmail.com>
2020-04-24 13:59:17 +0900

    implement credential repository

---
+++ infra/db/user/credential/credential.go
@@ -0,0 +1,68 @@
+package credential
+
+import (
+	"fmt"
+
+	credentialModel "github.com/team-gleam/kiwi-basket/domain/model/user/credential"
+	"github.com/team-gleam/kiwi-basket/domain/model/user/token"
+	"github.com/team-gleam/kiwi-basket/domain/model/user/username"
+	credentialRepository "github.com/team-gleam/kiwi-basket/domain/repository/user/credential"
+	"github.com/team-gleam/kiwi-basket/infra/db/handler"
+)
+
+type CredentialRepository struct {
+	dbHandler *handler.DbHandler
+}
+
+func NewCredentialRepository(h *handler.DbHandler) credentialRepository.ICredentialRepository {
+	credentialRepository := CredentialRepository{h}
+	return &credentialRepository
+}
+
+type authDB struct {
+	username string `gorm:"primary_key"`
+	token    string `gorm:"primary_key"`
+}
+
+func transformAuthForDB(a credentialModel.Auth) authDB {
+	return authDB{a.Username().Name(), a.Token().Token()}
+}
+
+func toAuth(a authDB) (credentialModel.Auth, error) {
+	u, err := username.NewUsername(a.username)
+	return credentialModel.NewAuth(u, token.NewToken(a.token)), err
+}
+
+func (r *CredentialRepository) Append(a credentialModel.Auth) error {
+	d := transformAuthForDB(a)
+	return r.dbHandler.Db.Create(d).Error
+}
+
+func (r *CredentialRepository) Remove(a credentialModel.Auth) error {
+	d := transformAuthForDB(a)
+	return r.dbHandler.Db.Delete(d).Error
+}
+
+func (r *CredentialRepository) Exists(t token.Token) (bool, error) {
+	a := new(authDB)
+	err := r.dbHandler.Db.Where("token = ?", t.Token).First(&a).Error
+	if err != nil {
+		return false, err
+	}
+	return new(authDB) != a, nil
+}
+
+func (r *CredentialRepository) Get(t token.Token) (credentialModel.Auth, error) {
+	auth := new(authDB)
+	err := r.dbHandler.Db.Where("token = ?", t.Token).First(&auth).Error
+	if err != nil {
+		return credentialModel.Auth{}, err
+	}
+
+	a, err := toAuth(*auth)
+	if err.Error() == username.InvalidUsername {
+		return credentialModel.Auth{}, fmt.Errorf("user not found")
+	}
+
+	return a, nil
+}

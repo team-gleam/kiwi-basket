b4d90a46193c7fbc383e5a9ce501d94ab3fd228d
4afS <4afS.h1@gmail.com>
2020-04-26 11:39:41 +0900

    add getters

--- domain/model/task/task.go
+++ domain/model/task/task.go
@@ -25,8 +25,16 @@ func NewTask(id int, date, title string) (Task, error) {
 	return Task{id, d, title}, nil
 }
 
+func (t Task) ID() int {
+	return t.id
+}
+
 func (t Task) TextDate() string {
 	return t.date.Format(layout)
+}
+
+func (t Task) Date() time.Time {
+	return t.date
 }
 
 func (t Task) Title() string {

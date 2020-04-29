74164755b45203c53ae8e8b04159801ba0c6275f
4afS <4afS.h1@gmail.com>
2020-04-29 21:17:54 +0900

    implement a method to sign up

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.12
+	github.com/labstack/echo/v4 v4.1.16
 )
--- interfaces/controllers/user/login.go
+++ interfaces/controllers/user/login.go
@@ -1,7 +1,14 @@
 package login
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	loginModel "github.com/team-gleam/kiwi-basket/domain/model/user/login"
+	"github.com/team-gleam/kiwi-basket/domain/model/user/username"
 	loginRepository "github.com/team-gleam/kiwi-basket/domain/repository/user/login"
+	errorResponse "github.com/team-gleam/kiwi-basket/interfaces/controllers/error"
 	loginUsecase "github.com/team-gleam/kiwi-basket/usecase/user/login"
 )
 
@@ -13,4 +20,56 @@ func NewLoginController(r loginRepository.ILoginRepository) *LoginController {
 	return &LoginController{
 		loginUsecase.NewLoginUsecase(r),
 	}
+}
+
+type LoginResponse struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (l LoginResponse) toLogin() (loginModel.Login, error) {
+	u, err := username.NewUsername(l.Username)
+	if err != nil {
+		return loginModel.Login{}, err
+	}
+
+	return loginModel.NewLogin(u, l.Password), nil
+}
+
+const (
+	InvalidUsernameOrPassword = "invalid username or password"
+	InternalServerError       = "internal server error"
+)
+
+func (c LoginController) SignUp(ctx echo.Context) error {
+	login := new(LoginResponse)
+	err := ctx.Bind(login)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, errorResponse.NewError(err))
+	}
+	if login.Username == "" || login.Password == "" {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			errorResponse.NewError(fmt.Errorf(InvalidUsernameOrPassword)),
+		)
+	}
+
+	// this error has been checked, so we ignore it here.
+	l, _ := login.toLogin()
+
+	err = c.loginUsecase.Add(l)
+	if err.Error() == loginUsecase.UsernameAlreadyExists {
+		return ctx.JSON(
+			http.StatusConflict,
+			errorResponse.NewError(fmt.Errorf(loginUsecase.UsernameAlreadyExists)),
+		)
+	}
+	if err != nil {
+		return ctx.JSON(
+			http.StatusInternalServerError,
+			errorResponse.NewError(fmt.Errorf(InternalServerError)),
+		)
+	}
+
+	return ctx.NoContent(http.StatusOK)
 }

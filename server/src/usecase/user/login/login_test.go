a23b32f06bc2ebb7a098613ae9519887144578eb
4afS <4afs.h1@gmail.com>
2020-11-13 01:17:09 +0900

    add test for deleting login

--- server/src/usecase/user/login/login_test.go
+++ server/src/usecase/user/login/login_test.go
@@ -31,8 +31,8 @@ func TestAdd(t *testing.T) {
 
 		username, _ := username.NewUsername("user")
 		err := usecase.Add(login.NewLogin(username, ""))
-		if err.Error() != UsernameAlreadyExists {
-			t.Fatalf("expected: %v; got: %v\n", UsernameAlreadyExists, err)
+		if expected := UsernameAlreadyExists; err.Error() != expected {
+			t.Fatalf("expected: %v; got: %v\n", expected, err)
 		}
 	})
 
@@ -42,7 +42,7 @@ func TestAdd(t *testing.T) {
 		username, _ := username.NewUsername("user")
 		err := usecase.Add(login.NewLogin(username, ""))
 		if err == nil {
-			t.Fatalf("unexpected error but got nil")
+			t.Fatalf("expected error but got nil")
 		}
 	})
 
@@ -53,7 +53,55 @@ func TestAdd(t *testing.T) {
 		username, _ := username.NewUsername("user")
 		err := usecase.Add(login.NewLogin(username, ""))
 		if err == nil {
-			t.Fatalf("unexpected error but got nil")
+			t.Fatalf("expected error but got nil")
+		}
+	})
+}
+
+func TestDelete(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	loginRepository := mocks.NewMockILoginRepository(ctrl)
+	usecase := NewLoginUsecase(loginRepository)
+
+	t.Run("success", func(t *testing.T) {
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, nil)
+		loginRepository.EXPECT().Delete(gomock.Any()).Return(nil)
+
+		username, _ := username.NewUsername("user")
+		err := usecase.Delete(login.NewLogin(username, ""))
+		if err != nil {
+			t.Fatalf("expected error: %v\n", err)
+		}
+	})
+
+	t.Run("user not found", func(t *testing.T) {
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(false, nil)
+
+		username, _ := username.NewUsername("user")
+		err := usecase.Delete(login.NewLogin(username, ""))
+		if expected := UsernameNotFound; err.Error() != expected {
+			t.Fatalf("expected: %v; got: %v\n", expected, err)
+		}
+	})
+
+	t.Run("Exists return error", func(t *testing.T) {
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, fmt.Errorf("error occurred"))
+
+		username, _ := username.NewUsername("user")
+		err := usecase.Delete(login.NewLogin(username, ""))
+		if err == nil {
+			t.Fatalf("expected error but got nil")
+		}
+	})
+
+	t.Run("Delete return error", func(t *testing.T) {
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, nil)
+		loginRepository.EXPECT().Delete(gomock.Any()).Return(fmt.Errorf("error occurred"))
+
+		username, _ := username.NewUsername("user")
+		err := usecase.Delete(login.NewLogin(username, ""))
+		if err == nil {
+			t.Fatalf("expected error but got nil")
 		}
 	})
 }

e2369f56485848e55167bd8813a71a09fc3da125
4afS <4afs.h1@gmail.com>
2020-11-12 22:41:25 +0900

    use Fatalf instead of Errorf

--- server/src/domain/model/user/token/token_test.go
+++ server/src/domain/model/user/token/token_test.go
@@ -6,11 +6,11 @@ func TestGenToken(t *testing.T) {
 	t.Run("length of token", func(t *testing.T) {
 		token, err := GenToken()
 		if err != nil {
-			t.Errorf("unexpected error: %v", err)
+			t.Fatalf("unexpected error: %v", err)
 		}
 
 		if l := len(token.Token()); l != Length {
-			t.Errorf("Failed# expected: %v; got: %v\n", Length, l)
+			t.Fatalf("Failed# expected: %v; got: %v\n", Length, l)
 		}
 	})
 }

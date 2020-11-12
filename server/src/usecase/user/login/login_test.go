cc017e03b57d5d688673a2b83094ce82cb9f28dd
4afS <4afs.h1@gmail.com>
2020-11-13 01:32:03 +0900

    add test for verifying

--- server/src/usecase/user/login/login_test.go
+++ server/src/usecase/user/login/login_test.go
@@ -105,3 +105,83 @@ func TestDelete(t *testing.T) {
 		}
 	})
 }
+
+func TestVerify(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	loginRepository := mocks.NewMockILoginRepository(ctrl)
+	usecase := NewLoginUsecase(loginRepository)
+
+	t.Run("success", func(t *testing.T) {
+		username, _ := username.NewUsername("user")
+		password := "password"
+		login := login.NewLogin(username, password)
+
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, nil)
+		loginRepository.EXPECT().Get(gomock.Any()).Return(login, nil)
+
+		v, err := usecase.Verify(login)
+		if err != nil {
+			t.Fatalf("expected error: %v\n", err)
+		}
+		if !v {
+			t.Fatalf("expected: %v; got: %v\n", true, err)
+		}
+	})
+
+	t.Run("user not found", func(t *testing.T) {
+		username, _ := username.NewUsername("user")
+		password := "password"
+		login := login.NewLogin(username, password)
+
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(false, nil)
+
+		_, err := usecase.Verify(login)
+		if expected := UsernameNotFound; err.Error() != expected {
+			t.Fatalf("expected: %v; got: %v\n", expected, err)
+		}
+	})
+
+	t.Run("invalid password", func(t *testing.T) {
+		username, _ := username.NewUsername("user")
+		password := "password1"
+		l := login.NewLogin(username, password)
+
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, nil)
+		loginRepository.EXPECT().Get(gomock.Any()).Return(login.NewLogin(username, "password2"), nil)
+
+		v, err := usecase.Verify(l)
+		if err != nil {
+			t.Fatalf("expected error: %v\n", err)
+		}
+		if v {
+			t.Fatalf("expected: %v; got: %v\n", false, err)
+		}
+	})
+
+	t.Run("Exists return error", func(t *testing.T) {
+		username, _ := username.NewUsername("user")
+		password := "password"
+		l := login.NewLogin(username, password)
+
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, fmt.Errorf("error occurred"))
+
+		_, err := usecase.Verify(l)
+		if err == nil {
+			t.Fatalf("expected error but got nil")
+		}
+	})
+
+	t.Run("Get return error", func(t *testing.T) {
+		username, _ := username.NewUsername("user")
+		password := "password"
+		l := login.NewLogin(username, password)
+
+		loginRepository.EXPECT().Exists(gomock.Any()).Return(true, nil)
+		loginRepository.EXPECT().Get(gomock.Any()).Return(l, fmt.Errorf("error occurred"))
+
+		_, err := usecase.Verify(l)
+		if err == nil {
+			t.Fatalf("expected error but got nil")
+		}
+	})
+}
